ec99d941f52ffb91118e42982660241bbddff42f
Carlos <lelouchvibrittaniagoat@gmail.com>
2025-08-25 20:12:21 -0300

    Atualizado

--- CR/CR/obj/Debug/net9.0-android/assets/wwwroot/downloadFile.js
+++
@@ -1,8 +0,0 @@
-window.downloadFileFromBase64 = (filename, base64Data) => {
-    const link = document.createElement('a');
-    link.download = filename;
-    link.href = "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64," + base64Data;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-};
